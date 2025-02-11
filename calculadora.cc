6f56fbf54cb57f301ec3f802cc542c2804546407
vinycius <vinyciusalves155@gmail.com>
2025-02-11 12:34:26 -0300

    Adicao do codigo

---
+++ calculadora.cc
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+// --- VERIFICAR QUAL OP TEM MAIOR PIORIDADE
+int prioridade(char op)
+{
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == 'x' || op == '/')
+        return 2;
+    return 0;
+}
+
+// - REALIZA AS OPERACOES
+double calcular(int num1, int num2, char op)
+{
+    switch (op)
+    {
+    case '+':
+        return num1 + num2;
+    case '-':
+        return num1 - num2;
+    case '*':
+        return num1 * num2;
+    case 'x':
+        return num1 * num2;
+    case '/':
+        return num1 / num2;
+    default:
+        return 0;
+    }
+}
+
+// ---- ORGANIZA OS NUMEROS E CALCULA EM ORDEM
+double realizarCalculo(string calculo)
+{
+    stack<double> numeros;
+    stack<char> operadores;
+
+    for (size_t i = 0; i < calculo.length(); i++)
+    {
+        char caractere = calculo[i];
+        if (isspace(caractere))
+            continue;
+
+        if (isdigit(caractere))
+        {
+            double numero = 0;
+            while (i < calculo.length() && isdigit(calculo[i]))
+            {
+                numero = numero * 10 + (calculo[i] - '0');
+                i++;
+            }
+            i--;
+            numeros.push(numero);
+        }
+        else if (caractere == '(')
+        {
+            operadores.push(caractere);
+        }
+        else if (caractere == ')')
+        {
+            while (!operadores.empty() && operadores.top() != '(')
+            {
+                double num2 = numeros.top();
+                numeros.pop();
+                double num1 = numeros.top();
+                numeros.pop();
+                char op = operadores.top();
+                operadores.pop();
+                numeros.push(calcular(num1, num2, op));
+            }
+            operadores.pop();
+        }
+        else
+        {
+            while (!operadores.empty() && prioridade(operadores.top()) >= prioridade(caractere))
+            {
+                double num2 = numeros.top();
+                numeros.pop();
+                double num1 = numeros.top();
+                numeros.pop();
+                char op = operadores.top();
+                operadores.pop();
+                numeros.push(calcular(num1, num2, op));
+            }
+            operadores.push(caractere);
+        }
+    }
+    while (!operadores.empty())
+    {
+        double num2 = numeros.top();
+        numeros.pop();
+        double num1 = numeros.top();
+        numeros.pop();
+        char op = operadores.top();
+        operadores.pop();
+        numeros.push(calcular(num1, num2, op));
+    }
+    return numeros.top();
+}
+
+// ------ FUNCAO PRINCIPAL
+int main()
+{
+    string calculo;
+    cout << "calculadora v1.1 (Divisoes por 0 e calculos decimais fora de servico)\n\nInforme o calculo que deseja realizar: \n";
+    getline(cin, calculo);
+
+    double resultado = realizarCalculo(calculo);
+
+    cout << "Resultado: " << resultado;
+    return 0;
+}
